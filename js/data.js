44f57ebc4eacc2c86c957d8a518a72eca2780a57
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-25 00:16:59 +0500

    Add categories section

---
+++ js/data.js
@@ -0,0 +1,49 @@
+const data = {
+  categories: [
+    {
+      icon: "assets/icons/icon-burger.svg",
+      alt: "icon burger",
+      text: "Бургеры",
+    },
+    {
+      icon: "assets/icons/icon-snacks.svg",
+      alt: "icon snacks",
+      text: "Закуски",
+    },
+    {
+      icon: "assets/icons/icon-hot-dog.svg",
+      alt: "icon hot-dog",
+      text: "Хот-доги",
+    },
+    {
+      icon: "assets/icons/icon-combo.svg",
+      alt: "icon combo",
+      text: "Комбо",
+    },
+    {
+      icon: "assets/icons/icon-shaurma.svg",
+      alt: "icon shaurma",
+      text: "Шаурма",
+    },
+    {
+      icon: "assets/icons/icon-pizza.svg",
+      alt: "icon pizza",
+      text: "Пицца",
+    },
+    {
+      icon: "assets/icons/icon-pasta.svg",
+      alt: "icon pasta",
+      text: "Вок",
+    },
+    {
+      icon: "assets/icons/icon-desert.svg",
+      alt: "icon deserts",
+      text: "Десерты",
+    },
+    {
+      icon: "assets/icons/icon-ketchup.svg",
+      alt: "icon ketchup",
+      text: "Соусы",
+    },
+  ],
+};
